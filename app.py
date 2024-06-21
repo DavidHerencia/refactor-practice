4028ad3ea6a183f92a3f789385525bf23f9ca237
DavidHerencia <david.herencia@utec.edu.pe>
2024-06-20 19:40:12 -0500

    refactor

--- app.py
+++ app.py
@@ -7,44 +7,80 @@ import csv
 # el DNI debe ser valido (8 digitos)
 
 class CalculaGanador:
+    def __init__(self):
+        self.CANDIDATO = None
+        self.ESVALIDO = None
 
     # Tecnica de parametrizacion: se añade un parametro nombre_archivo para poder leer cualquier archivo (hace el codigo más flexible)
-    def leer_csv(self, nombre_archivo = '0204.csv'): 
-        """Leer datos de un archivo"""  
+    # Tecnica de Renombre: se renombra la funcion leer_datos a cargar_datos_de_csv que explica mejor lo que hace el metodo
+    def cargar_datos_de_csv(self, nombre_archivo = '0204.csv'):  
         data = []
         try:
             with open(nombre_archivo, 'r') as csvfile:
-                next(csvfile) # saltar la primera fila (headers)
+                # Parsear encabezado
+                encabezado = csvfile.readline().strip().split(',')
+                self.parse_encabezado(encabezado)  # Tecnica de Extraccion de metodo 
+                # Leer filas
                 datareader = csv.reader(csvfile)
                 for fila in datareader:
                     data.append(fila)
-            
-        except FileNotFoundError:
-            print(f"El archivo {nombre_archivo} no se encontre")
-        
-        return data
 
+        except FileNotFoundError:
+            print(f"No se encontro el archivo {nombre_archivo}")
+        except ValueError as ve:
+            print(f"No se encontro la columna {ve}")
+
+        return data
 
     def calcularganador(self, data):
         votosxcandidato = {}
-        for fila in data:
-            if not fila[4] in votosxcandidato:
-                votosxcandidato[fila[4]] = [fila[4], 0]
-            if fila[5] == '1':
-                votosxcandidato[fila[4]][1] = votosxcandidato[fila[4]][1] + 1
-        ordenado = sorted(votosxcandidato.items(), key=lambda item:item[1][1], reverse=True)
-        for candidato in votosxcandidato:
-            print('candidato: ' + candidato + ' votos validos: ' + str(votosxcandidato[candidato]))
-        for candidato in ordenado:
-            return [candidato]
 
+        VALIDO = '1' # Tecnica de introducir variable descripitiva
+        CONTEO_VOTOS = 0 # Tecnica de introducir variable descripitiva
+
+        for fila in data:
+            if not fila[self.CANDIDATO] in votosxcandidato:
+                votosxcandidato[fila[self.CANDIDATO]] = 0
+
+            if fila[self.ESVALIDO] == VALIDO:
+                votosxcandidato[fila[self.CANDIDATO]] += 1
+                CONTEO_VOTOS += 1
+
+        # Tecnica de renombre: se renombra la variable ordenado a resultados_ordenados en base al value
+        resultados_ordenados = sorted(votosxcandidato, key=votosxcandidato.get, reverse=True)
+
+        # Tecnica de extraccion de metodo: se extrae el metodo imprimir_resultados
+        self.imprimir_resultados(votosxcandidato)
+
+        # Tecnica de sustituir de codigo reduntante: se elimino el for innecesario
+
+        # Tecnica de extraccion de metodo: se extrae el metodo definir_resultado
+        return self.definir_resultado(resultados_ordenados,CONTEO_VOTOS, votosxcandidato[resultados_ordenados[0]])
+
+    def parse_encabezado(self, encabezado):
+        # Tecnica de introducir variable descripitiva
+        self.CANDIDATO = encabezado.index('candidato')
+        self.ESVALIDO = encabezado.index('esvalido')
+        if self.CANDIDATO == -1 or self.ESVALIDO == -1:
+            raise ValueError("Candidato o Esvalido en el archivo CSV ")
+
+    def definir_resultado(self, resultados_ordenado, CONTEO_VOTOS, votos_ganador):
+        if votos_ganador > CONTEO_VOTOS / 2:
+            return [resultados_ordenado[0]]        
+        return [resultados_ordenado[0], resultados_ordenado[1]]
+
+    def imprimir_resultados(self, votosxcandidato):
+        for candidato in votosxcandidato:
+            print(f"Candidato {candidato} tiene {votosxcandidato[candidato]} votos")
 
 c = CalculaGanador()
-print(c.calcularganador(c.leerdatos()))
+print("Resultados :", c.calcularganador(c.cargar_datos_de_csv()))
+
+print()
 datatest = [
 ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '0'],
 ['Áncash', 'Asunción', 'Acochaca', '57533597', 'Eddie Hinesley', '1'],
 ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '1'],
 ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '1']
 ]
-print(c.calcularganador(datatest))
+print("Resultados :",c.calcularganador(datatest))
